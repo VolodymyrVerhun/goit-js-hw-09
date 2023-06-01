cd9d79e6b71660f0abc88efc472c4064e9393632
volodymyr1v <volodumur.verhyn@gmail.com>
2023-06-01 22:13:20 +0300

    second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 
 // отримаю посилання
@@ -10,7 +11,31 @@ const hourEl = document.querySelector('[data-hours]');
 const minuteEl = document.querySelector('[data-minutes]');
 const secondEl = document.querySelector('[data-seconds]');
 
-let currentTime = new Date();
+function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+};
+
+function convertMs(ms) {
+        // Number of milliseconds per unit of time
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+      
+        // Remaining days
+        const days = Math.floor(ms / day);
+        // Remaining hours
+        const hours = Math.floor((ms % day) / hour);
+        // Remaining minutes
+        const minutes = Math.floor(((ms % day) % hour) / minute);
+        // Remaining seconds
+        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+      
+        return { days, hours, minutes, seconds };
+      }
+
+
+
 // функція з бібліотеки
 flatpickr(calendarEl, {
     enableTime: true,
@@ -18,7 +43,7 @@ flatpickr(calendarEl, {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-    
+        let currentTime = new Date();
     // перевірка на час
     if(selectedDates[0].getTime() > currentTime.getTime()) {
         btnEl.disabled = false;
@@ -28,31 +53,22 @@ flatpickr(calendarEl, {
 
                 let nowDate = Date.now();
                 let targetDate = selectedDates[0].getTime();
-                let deltadate = Math.round((targetDate - nowDate) / 1000);
+                let deltadate = targetDate - nowDate;
 
-                let seconds = deltadate % 60;
-                let minutesTotal = Math.floor((deltadate / 60));
-                let minutes = minutesTotal % 60;
-                let hoursTotal = Math.floor((minutesTotal / 60));
-                let hours = hoursTotal % 24;
-                let days = Math.floor(hoursTotal / 24);
                 
-                // // записую числове значення дати у кожен елемент
-                dayEl.textContent = days.toString().padStart(2, '0');
-                hourEl.textContent = hours.toString().padStart(2, '0');
-                minuteEl.textContent = minutes.toString().padStart(2, '0');
-                secondEl.textContent = seconds.toString().padStart(2, '0');
+                const { days, hours, minutes, seconds } = convertMs(deltadate);
+              
+                dayEl.textContent = addLeadingZero(days);
+                hourEl.textContent = addLeadingZero(hours);
+                minuteEl.textContent = addLeadingZero(minutes);
+                secondEl.textContent = addLeadingZero(seconds);
 
             }, 1000);
         })
     } else {
         btnEl.disabled = true;
-        return window.alert("Please choose a date in the future");
+        return Notiflix.Notify.failure('Please choose a date in the future');
         
     }
     },
   });
-
-
-
-
